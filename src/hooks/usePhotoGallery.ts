52a023c7e164227a06ac4407d435f5ba922190c8
Lionel <lio4455@hotmail.com>
2022-04-02 10:59:42 +0200

    app takes pictures and saves it to filesystem

---
+++ src/hooks/usePhotoGallery.ts
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react';
+import { isPlatform } from '@ionic/react';
+
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+import { Capacitor } from '@capacitor/core';
+
+//Type definition
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
+export function usePhotoGallery() {
+  //save photo to file system
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
+
+  //util that downloads a file from the supplied path and returns a base64 representation of that file.
+  async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string');
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  const takePhoto = async () => {
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    //store photos into the array
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFileImage = await savePicture(photo, fileName);
+    const newPhotos = [savedFileImage, ...photos];
+    setPhotos(newPhotos);
+  };
+
+  return {
+    photos,
+    takePhoto,
+  };
+}
